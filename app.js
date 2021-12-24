c31e3d4d82221a4d45b6c6b30993bfb15ec2d8f9
prabhat148 <78421243+prabhat148@users.noreply.github.com>
2021-12-24 21:01:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 
 app.get('/',function(req,res){
 
-    res.sendFile(__dirname +'/signup.html')
+    res.sendFile(__dirname +'/index.html')
   
 })
 
